617a2faf7f129f6c41717c0456e3423632393aed
willyandie23 <willyandie.sc@gmail.com>
2025-06-07 21:10:03 +0700

    Create view Dashboard Backend

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Models\News;
+use App\Models\Field;
+use App\Models\Contact;
+use App\Models\Download;
+use App\Models\Organization;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +14,27 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.dashboard.index');
+        // Mengambil jumlah Organization
+        $organizationCount = Organization::count();
+
+        // Mengambil jumlah News
+        $newsCount = News::count();
+
+        // Mengambil jumlah Field
+        $fieldCount = Field::count();
+
+        // Mengambil jumlah Downloads
+        $downloadCount = Download::sum('total_download');
+
+        // Mengambil dua pesan terbaru dari Contact
+        $latestContacts = Contact::latest()->take(2)->get();
+
+        return view('backend.dashboard.index', compact(
+            'organizationCount', 
+            'newsCount', 
+            'fieldCount', 
+            'downloadCount', 
+            'latestContacts'
+        ));
     }
 }
